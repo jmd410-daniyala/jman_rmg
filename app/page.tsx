2c196522c0754acea02a766695a895928ea4571f
Daniyala Kiran Kumar <daniyala.k@jmangroup.com>
2026-06-25 15:56:24 +0530

    chore: initialize Next.js project setup

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+const page = () => {
+  return (
+    <div>
+      <h1>JMAN JSpark Hackathon 2.0</h1>
+      <p>Welcome!</p>
+      <p>
+        This is the README file for the JMAN JSpark Hackathon 2.0.
+      </p>
+      <p>
+        This repository is the central Git repository for Team Phoenix and
+        contains all project-related code, documentation, and resources.
+      </p>
+    </div>
+  );
+}
+
+export default page
